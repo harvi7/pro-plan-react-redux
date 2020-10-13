c2f3cb45e08ca50c29665a4e313aebe165cee9f0
Harsh Virani <iamharshvirani7@gmail.com>
2020-10-12 23:04:55 -0700

    Firbase Auth, Login and Logout

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import authReucer from './authReducer'
 import projectReducer from './projectReducer'
 import { combineReducers } from 'redux'
 import { firestoreReducer } from 'redux-firestore'
+import { firebaseReducer } from 'react-redux-firebase'
 
 const rootReducer = combineReducers({ 
     auth: authReucer,
     project: projectReducer,
-    firestore: firestoreReducer 
+    firestore: firestoreReducer,
+    firebase: firebaseReducer 
 })
 
 export default rootReducer
